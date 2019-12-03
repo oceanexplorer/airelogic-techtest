a4a850fc1e39d41207f10f1d5a8cfdc9d780a6b9
Paul Custance <pcustance@gmail.com>
2019-12-03 21:35:22 +0000

    Upgrade to .NET Core 3.0

--- src/BugTraq.Api/Startup.cs
+++ src/BugTraq.Api/Startup.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using BugTraq.Api.Models;
 
@@ -27,7 +20,7 @@ namespace BugTraq.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddRazorPages();
             services.AddCors();
 
             var connection = "Data Source=bugs.db";
@@ -35,7 +28,7 @@ namespace BugTraq.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -43,13 +36,15 @@ namespace BugTraq.Api
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("/Error");
                 app.UseHsts();
             }
+            
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseCors(builder => builder.WithOrigins("http://localhost:8080").AllowAnyMethod());
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
-            // app.UseHttpsRedirection();
-            app.UseCors(builder => builder.WithOrigins("http://localhost:3000").AllowAnyMethod());
-            app.UseMvc();
         }
     }
 }
