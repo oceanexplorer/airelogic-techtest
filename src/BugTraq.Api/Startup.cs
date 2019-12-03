adcce4d525b51fa8cf508949485c0848fd74d0c3
Paul Custance <pcustance@gmail.com>
2019-12-03 21:45:42 +0000

    Upgrade to .NET Core 3.0

--- src/BugTraq.Api/Startup.cs
+++ src/BugTraq.Api/Startup.cs
@@ -20,7 +20,8 @@ namespace BugTraq.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllers();
+            services.AddHealthChecks();
             services.AddCors();
 
             var connection = "Data Source=bugs.db";
@@ -43,7 +44,11 @@ namespace BugTraq.Api
             app.UseStaticFiles();
             app.UseRouting();
             app.UseCors(builder => builder.WithOrigins("http://localhost:8080").AllowAnyMethod());
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
+            });
 
         }
     }
