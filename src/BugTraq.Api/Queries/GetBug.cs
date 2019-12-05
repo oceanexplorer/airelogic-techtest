71b12979b29eed1fea47e14557fb7bb721c4fd51
Paul Custance <pcustance@gmail.com>
2019-12-05 22:36:36 +0000

    Add a query to a get a single bug

---
+++ src/BugTraq.Api/Queries/GetBug.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using BugTraq.Api.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+// ReSharper disable UnusedMember.Global
+// ReSharper disable ClassNeverInstantiated.Global
+
+namespace BugTraq.Api.Queries
+{
+    public static class GetBug
+    {
+        public class Query : IRequest<Result>
+        {
+            public int Id { get; }
+
+            public Query(int id)
+            {
+                Id = id;
+            }
+        }
+
+        public class Result
+        {
+            public int BugId { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public DateTime CreatedDate { get; set; }
+            public string Status { get; set; }
+            public int UserId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result>
+        {
+            private readonly BugTraqContext _context;
+            private readonly IMapper _mapper;
+        
+            public Handler(BugTraqContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+            
+            public async Task<Result> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _mapper
+                    .ProjectTo<Result>(_context.Bugs)
+                    .Where(b => b.BugId == request.Id)
+                    .FirstOrDefaultAsync(cancellationToken);
+            }
+        }
+    }
+}
