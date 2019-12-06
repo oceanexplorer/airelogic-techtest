2e1426b130f3761e3ee9053628a69ba5622d5213
Paul Custance <pcustance@gmail.com>
2019-12-06 08:57:02 +0000

    Add FontAwesome to the project

--- src/bugtraq.presentation.vue/src/main.js
+++ src/bugtraq.presentation.vue/src/main.js
@@ -1,11 +1,18 @@
-import '@babel/polyfill';
-import 'mutationobserver-shim';
-import Vue from 'vue';
-import './plugins/bootstrap-vue';
-import App from './App.vue';
-import VueLodash from 'vue-lodash';
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import App from './App.vue'
+import VueLodash from 'vue-lodash'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 Vue.use(VueLodash);
