531e772d5d670371735b0755345e09995eb3db33
Paul Custance <pcustance@gmail.com>
2019-12-07 08:49:42 +0000

    Improve naming to match the use of the page

--- src/bugtraq.presentation.vue/src/router/index.js
+++ src/bugtraq.presentation.vue/src/router/index.js
@@ -1,22 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Bugs from '../views/Bugs.vue'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'bugs',
+    component: Bugs
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/users',
+    name: 'users',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Users.vue')
   }
 ]
 
