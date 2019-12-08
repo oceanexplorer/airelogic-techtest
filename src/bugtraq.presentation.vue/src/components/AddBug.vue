e0dbcb06958bdd0b36adf9cac0b342cd5f60a88f
Paul Custance <pcustance@gmail.com>
2019-12-08 18:42:56 +0000

    Add an error alert to the modal

--- src/bugtraq.presentation.vue/src/components/AddBug.vue
+++ src/bugtraq.presentation.vue/src/components/AddBug.vue
@@ -2,18 +2,117 @@
   <div>
     <b-button v-b-modal.modal-1>Add Bug</b-button>
 
-    <b-modal id="modal-1" title="BootstrapVue">
-      <p class="my-4">Hello from modal!</p>
+    <b-modal id="modal-1" title="Add A Bug" size="lg" busy="true">
+      <div>
+        <b-alert v-model="errored" variant="danger" dismissible>
+          Sorry an error has occured
+        </b-alert>
+        <b-form @submit="onSubmit" @reset="onReset" v-if="show" class="form-horizontal">
+          <b-form-group
+            id="title-group"
+            label="Title"
+            label-for="title"
+            label-cols="3"
+          >
+            <b-form-input
+              id="title"
+              v-model="form.title"
+              type="text"
+              required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            id="description-group"
+            label="Description"
+            label-for="description"
+            label-cols="3"
+          >
+            <b-form-input
+              id="description"
+              v-model="form.description"
+              type="text"
+              required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group 
+            id="status-group" 
+            label="Status" 
+            label-for="status"
+            label-cols="3"
+          >
+            <b-form-select
+              id="status"
+              v-model="form.status"
+              :options="statuses"
+              required
+            ></b-form-select>
+          </b-form-group>
+
+          <b-form-group
+            id="user-group"
+            label="Users"
+            label-for="user"
+            label-cols="3"
+          >
+            <b-form-select
+              id="user"
+              v-model="form.userId"
+              :options="users"
+              required
+            ></b-form-select>
+          </b-form-group>
+
+          <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="reset" variant="danger">Reset</b-button>
+        </b-form>
+      </div>
     </b-modal>
   </div>
 </template>
 
 <script>
     export default {
-        name: ""
+      data() {
+        return {
+          form: {
+            title: '',
+            description: '',
+            status: null,
+            userId: null
+          },
+          statuses: [{ text: 'Select One', value: null }, 'New', 'Active', 'Resolved', 'Closed'],
+          show: true,
+          users: [{ text: 'Select One', value: null }, { text: 'Sarah Smith', value: 1 }],
+          errored: false
+        }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault();
+        let self = this;
+        this.axios
+          .post('http://localhost:5000/api/bugs', this.form)
+          .catch(function () {
+            self.errored = true;
+          });
+      },
+      onReset(evt) {
+        evt.preventDefault();
+        
+        // Reset our form values
+        this.form.title = '';
+        this.form.description = '';
+        this.form.status = null;
+        this.form.userId = [];
+        
+        // Trick to reset/clear native browser form validation state
+        this.show = false;
+        this.$nextTick(() => {
+            this.show = true
+        })
+      }
     }
+  }
 </script>
-
-<style scoped>
-
-</style>
