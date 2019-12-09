4a75f7cdda1bd544acfb314b946cb157a0c64333
Paul Custance <pcustance@gmail.com>
2019-12-09 13:45:32 +0000

    Add test project and unit tests for the queries. Upgrade EF to latest version.

---
+++ src/BugTraq.Api/tests/BugTraq.Api.Tests/Bugs/QueryFacts.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BugTraq.Api.Models;
+using Moq;
+using Xunit;
+using BugTraq.Api.Queries;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using BugTraq.Api.Mappers;
+using FluentAssertions;
+
+namespace BugTraq.Api.Tests.Bugs
+{
+    public class QueryFacts
+    {
+        private DbContextOptions<BugTraqContext> _inMemoryDbOptions;
+        private IMapper _mapper;
+
+        public QueryFacts()
+        {
+           _inMemoryDbOptions = new DbContextOptionsBuilder<BugTraqContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+           
+           _mapper = new MapperConfiguration(cfg => { cfg.AddProfile(new BugProfile()); }).CreateMapper();
+        }
+        
+        public class GetBugsQuery : QueryFacts
+        {
+            [Fact]
+            public async Task ShouldReturnSameNumberOfRecordsAsAdded()
+            {
+                // Arrange
+                var user = new User {FirstName = "Sarah", Surname = "Smith"};
+                var bugOne = new Bug("This is bug one", "BugOne", "New", user.UserId);
+                var bugTwo = new Bug("This is big two", "BugTwo", "Closed", user.UserId);
+                var getBugsQuery = new GetBugs.Query();
+
+                await using (var dbContext = new BugTraqContext(_inMemoryDbOptions))
+                {
+                    dbContext.Add(user);
+                    dbContext.Add(bugOne);
+                    dbContext.Add(bugTwo);
+                    dbContext.SaveChanges();
+                }
+                
+                // Act
+                await using (var dbContext = new BugTraqContext(_inMemoryDbOptions))
+                {
+                    var bugs = await new GetBugs.Handler(dbContext,  _mapper)
+                        .Handle(getBugsQuery, It.IsAny<CancellationToken>());
+                    
+                    // Assert
+                    bugs.Count.Should().Be(2, "because two records were added to the database so two should have been returned");
+                }
+            }
+        }
+        
+        public class GetBugQuery : QueryFacts
+        {
+            public GetBugQuery()
+            {
+                _inMemoryDbOptions = new DbContextOptionsBuilder<BugTraqContext>()
+                    .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                    .Options;
+                
+                _mapper = new MapperConfiguration(cfg => { cfg.AddProfile(new BugProfile()); }).CreateMapper();
+            }
+        
+            [Fact]
+            public async Task ShouldReturnOnlyOneRecordWithAMatchingId()
+            {
+                // Arrange
+                var user = new User {FirstName = "Sarah", Surname = "Smith"};
+                var bugOne = new Bug("This is bug one", "BugOne", "New", user.UserId);
+                var bugTwo = new Bug("This is big two", "BugTwo", "Closed", user.UserId);
+                var getBugQuery = new GetBug.Query(bugOne.BugId);
+                
+                await using (var dbContext = new BugTraqContext(_inMemoryDbOptions))
+                {
+                    dbContext.Add(bugOne);
+                    dbContext.Add(bugTwo);
+                    dbContext.SaveChanges();
+                }
+                
+                // Act
+                await using (var dbContext = new BugTraqContext(_inMemoryDbOptions))
+                {
+                    var bug = await new GetBug.Handler(dbContext, _mapper)
+                        .Handle(getBugQuery, It.IsAny<CancellationToken>());
+                    
+                    // Assert
+                    bug.Should().BeEquivalentTo(bugOne, "because we passed the id of bug one to the query " +
+                                                        "which should return the record with a matching id");
+                }
+            }
+        }
+    }
+}
