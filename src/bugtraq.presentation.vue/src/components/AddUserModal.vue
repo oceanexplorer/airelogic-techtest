e2850844820d9ef15d3ce839cd509b5e284d04ed
Paul Custance <pcustance@gmail.com>
2019-12-09 20:13:25 +0000

    Users page should have functionality to add a user

---
+++ src/bugtraq.presentation.vue/src/components/AddUserModal.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <b-button v-b-modal.addUserModal>Add User</b-button>
+
+    <b-modal id="addUserModal" title="Add A User" size="lg" hide-footer v-on:hidden="modalHidden">
+      <div>
+        <b-alert v-model="success" variant="success" dismissible>
+          User has been added successfully
+        </b-alert>
+        <b-alert v-model="errored" variant="danger" dismissible>
+          Sorry an error has occured
+        </b-alert>
+        <b-form @submit="onSubmit" v-if="show" class="form-horizontal">
+          <b-form-group
+                  id="firstName-group"
+                  label="First Name"
+                  label-for="firstName"
+                  label-cols="3"
+          >
+            <b-form-input
+                    id="firstName"
+                    v-model="form.firstName"
+                    type="text"
+                    required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+                  id="surname-group"
+                  label="Surname"
+                  label-for="surname"
+                  label-cols="3"
+          >
+            <b-form-input
+                    id="surname"
+                    v-model="form.surname"
+                    type="text"
+                    required
+            ></b-form-input>
+          </b-form-group>
+          <div class="float-right">
+            <b-button type="reset" variant="danger" class="mr-2" @click.prevent="close">Close</b-button>
+            <b-button type="submit" variant="primary">Submit</b-button>
+          </div>
+        </b-form>
+      </div>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                form: {
+                    firstName: '',
+                    surname: ''
+                },               
+                show: true,
+                errored: false,
+                success: false
+            }
+        },
+        methods: {
+            onSubmit(evt) {
+                evt.preventDefault();
+                let self = this;
+                this.axios
+                    .post('http://localhost:5000/api/users', this.form)
+                    .then(function() {
+                        self.success = true;
+                        self.reset();
+                        self.$root.$emit("user-added");
+                    })
+                    .catch(function () {
+                        self.errored = true;
+                    })
+            },
+            modalHidden() {
+                this.reset();
+                this.resetAlerts();
+            },
+            reset() {
+                // Reset our form values
+                this.form.title = '';
+                this.form.description = '';
+                this.form.status = null;
+                this.form.userId = null;
+
+                // Trick to reset/clear native browser form validation state
+                this.show = false;
+                this.$nextTick(() => {
+                    this.show = true
+                });
+            },
+            resetAlerts() {
+                this.errored = false;
+                this.success = false;
+            },
+            close () {
+                this.$bvModal.hide("addUserModal");
+            }
+        }
+    }
+</script>
--- src/bugtraq.presentation.vue/src/components/UsersTable.vue
+++ src/bugtraq.presentation.vue/src/components/UsersTable.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
-    <h2>Users</h2>
-    <div>
-      <b-table striped hover :items="users" :fields="fields" class="mt-4"></b-table>
+    <div class="row">
+      <div class="col">
+        <h2>Users</h2>      
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="col">
+        <add-user-modal class="float-right"/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <b-table striped hover :items="users" :fields="fields" class="mt-4"></b-table>        
+      </div>      
     </div>
   </div>
 </template>
 
 <script>
+    import AddUserModal from "./AddUserModal";
+    
     export default {
         name: "UsersTable",
+        components: {
+          AddUserModal        
+        },
         data () {
             return {
                 fields: ['firstName', 'surname'],
